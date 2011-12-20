107ee11fb906605f1987c73057621e8467e48fc5
Karl Uhlig <knuhlig@stanford.edu>
2011-12-19 16:23:53 -0800

    updated example, ignore ~ files

--- example/src/example.cpp
+++ example/src/example.cpp
@@ -1,5 +1,6 @@
 #include <ros/ros.h>
 #include <msg_util/Msg.h>
+#include <std_msgs/String.h>
 
 #define NODE_NAME "example_node"
 
@@ -8,13 +9,23 @@ int main(int argc, char** argv){
   ros::init(argc, argv, NODE_NAME);
   ros::NodeHandle nh;
   
+  // create a publisher
+  ros::Publisher pub = nh.advertise<std_msgs::String>("chatter", 0);
+  
   ros::Rate loop_rate(10.0);
   int count = 0;
+  char strbuf[1024];
   
   while (ros::ok()) {
-    Msg::publishString("chatter", "Hello world!");
-    count++;
+    //Msg::publishString("chatter", "Hello world!");
     
+    // publish the message
+    std_msgs::String msg;
+    sprintf(strbuf, "Hello world %d!", count);
+    msg.data = strbuf;
+    pub.publish(msg);
+    
+    count++;    
     loop_rate.sleep();
   }
   
